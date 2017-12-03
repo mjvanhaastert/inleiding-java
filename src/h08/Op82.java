fee919968715148f4c1833efb7c530e6ab5ca73e
Mark Jeff van Haastert <mjvanhaastert@hotmail.com>
2017-12-03 23:23:27 +0100

    opdracht 8.1

---
+++ src/h08/Op82.java
@@ -0,0 +1,14 @@
+package h08;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Op82 extends Applet {
+
+    Button knopleerlingjongen;
+    Button knopman;
+    Button knopleerlingvrouw;
+    Button knopvrouw;
+
+
+}
